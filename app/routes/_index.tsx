4e9e9125058288db988ed49984aa02f9e7732ecd
CherylZ4 <112735556+CherylZ4@users.noreply.github.com>
2023-09-12 11:28:08 -0400

    Changed About me paragraph

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -66,7 +66,7 @@ export default function Index() {
           exit="hide"
           variants={fadeIn}
           viewport={{once: true}} className="ml-5">
-          <div className="flex flex-col items-center justify-start ml-28 sm:ml-88 md:ml-80 lg:ml-96 xl:ml-96 mt-40 sm:mt-16 md:mt-16 lg:mt-16 xl:mt-16">
+          <div className="flex flex-col items-center justify-start ml-40 sm:ml-88 md:ml-80 lg:ml-96 xl:ml-96 mt-40 sm:mt-16 md:mt-16 lg:mt-16 xl:mt-16">
           
               <h1 className="text-custom-gray font-brygada-1918 text-3xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl font-medium lg:ml-24 xl:ml-24 lg:mt-10 xl:mt-10">About me</h1>
              
@@ -84,7 +84,7 @@ export default function Index() {
           <div className="flex-col-reverse sm:flex-row md:flex-row lg:flex-row xl:flex-row ml-6 sm:ml-44 md:ml-44 lg:ml-60 xl:ml-60 flex items-center md:mr-20 lg:mr-32 xl:mr-32">
          
               <p className="text-custom-gray font-brygada-1918 text-lg text-center mt-12">
-                My name is Cheryl Zhang, and I am pursuing a Specialist in Computer Science at the University of Toronto Scarborough. I am someone who enjoys continuous learning and adaptability. Furthermore, I embrace each new challenge as an opportunity to grow, and I'm excited to keep crafting innovative solutions that exceed expectations. Please feel free to reach out to chat!
+                My name is Cheryl Zhang, and I am pursuing a HBSc Computer Science Co-op Student at the University of Toronto Scarborough. I am someone who embraces each new challenge as an opportunity to grow, and I'm excited to keep crafting innovative solutions that exceed expectations. In my spare time, I enjoy reading manga and watching K-pop, makeup, and fashion videos. Please feel free to reach out to chat!
               </p>
               <img src={me} className='w-1/4 h-1/4 lg:w-1/5 lg:h-1/5 xl:w-1/5 xl:h-1/5 mb-0 md:mb-0 lg:mb-0 xl:mb-0 mt-10 sm:ml-20 md:ml-20 lg:ml-20 xl:ml-20'></img>
           </div>
